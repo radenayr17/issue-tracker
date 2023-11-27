041b0af52ccc927088ddce0269c4c7d8775bd595
cesarryan <cesar.edar@stationfive.com>
2023-11-27 21:11:22 +0800

    Handle error in deleting issue

--- app/issues/[id]/components/DeleteIssueButton.tsx
+++ app/issues/[id]/components/DeleteIssueButton.tsx
@@ -3,32 +3,45 @@ import { useDelete } from "@/app/issues/hooks/delete";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ id }: { id: string }) => {
-  const { onDelete } = useDelete(id);
+  const { onDelete, loading, error, clearError } = useDelete(id);
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action cannot be undone.
-        </AlertDialog.Description>
-        <Flex mt="4" gap="4">
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="4">
+            <AlertDialog.Cancel>
+              <Button color="gray" variant="soft" disabled={loading}>
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={onDelete} disabled={loading}>
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={!!error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>{error}</AlertDialog.Description>
           <AlertDialog.Cancel>
-            <Button color="gray" variant="soft">
+            <Button color="gray" variant="soft" mt="4" onClick={clearError}>
               Cancel
             </Button>
           </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color="red" onClick={onDelete}>
-              Delete Issue
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
--- app/issues/hooks/delete.ts
+++ app/issues/hooks/delete.ts
@@ -5,9 +5,13 @@ import { useState } from "react";
 export const useDelete = (id: string) => {
   const { push, refresh } = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const clearError = () => setError("");
 
   const onDelete = async () => {
     try {
+      setError("");
       setLoading(true);
 
       await axios.delete(`/api/issues/${id}`);
@@ -15,9 +19,13 @@ export const useDelete = (id: string) => {
       push("/issues");
       refresh();
     } catch (err: any) {
+      const error: Error = err as Error;
+
+      setError(error.message);
+
       setLoading(false);
     }
   };
 
-  return { onDelete, loading };
+  return { onDelete, loading, error, clearError };
 };
